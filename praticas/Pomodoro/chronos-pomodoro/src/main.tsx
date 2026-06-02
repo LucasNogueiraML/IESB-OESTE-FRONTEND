bac6e155611961af428ff844ae0872b4837701d8
unknown <24114290093@ACADEMICO.IESB.BR>
2026-06-02 09:38:14 -0300

    correções e pratica 03, adicionei minha antiga pasta pomodoro que eu estava fazendo antes da correção no git, por isso algumas coisas estão mais avançadas

--- praticas/Pomodoro/chronos-pomodoro/src/main.tsx
+++ praticas/Pomodoro/chronos-pomodoro/src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App' // <-- O segredo das chaves { }
 
-export function App() {
-  return (
-    <>
-      <h1>Título</h1>
-      <p>Parágrafo</p>
-    </>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)

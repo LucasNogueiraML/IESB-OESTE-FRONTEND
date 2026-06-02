e41f3838929d293d4879d9bc45d5f222f8770826
unknown <24114290093@ACADEMICO.IESB.BR>
2026-06-02 09:38:14 -0300

    correções e pratica 03, adicionei minha antiga pasta pomodoro que eu estava fazendo antes da correção no git, por isso algumas coisas estão mais avançadas

---
+++ praticas/Pomodoro/chronos-pomodoro/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+
+// 1º: Importamos as variáveis (o tema)
+import './styles/theme.css';
+// 2º: Importamos os estilos globais
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Olá, Mundo!</h1>
+      <p>Testando o CSS Global.</p>
+    </>
+  );
+}

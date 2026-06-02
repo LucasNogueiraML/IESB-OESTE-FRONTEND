9c85e9a9973903a7388e78497a663a8c0f201c98
unknown <24114290093@academico.iesb.br>
2026-06-02 08:53:11 -0300

    pratica 02 remoção de arquivos e interface basica criada

--- praticas/Pomodoro/chronos-pomodoro/src/main.tsx
+++ praticas/Pomodoro/chronos-pomodoro/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export function App() {
+  return (
+    <>
+      <h1>Título</h1>
+      <p>Parágrafo</p>
+    </>
+  );
+}

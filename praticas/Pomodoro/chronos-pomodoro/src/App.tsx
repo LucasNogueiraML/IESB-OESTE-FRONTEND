e9b7623dac7075b9269dc2c3febc4e26f7ee6fbc
LucasNogueiraML <lucas25070@gmail.com>
2026-06-09 13:01:18 -0300

    pratica 38 realizada

--- praticas/Pomodoro/chronos-pomodoro/src/App.tsx
+++ praticas/Pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,4 +1,5 @@
 import { Home } from './pages/Home';
+
 import './styles/theme.css';
 import './styles/global.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';

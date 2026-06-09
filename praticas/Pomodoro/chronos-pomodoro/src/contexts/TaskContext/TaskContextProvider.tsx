fe94611ba2c681328ffa4760bc46aa05899df28d
LucasNogueiraML <lucas25070@gmail.com>
2026-06-09 13:23:29 -0300

    pratica 54 realizada

--- praticas/Pomodoro/chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
+++ praticas/Pomodoro/chronos-pomodoro/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,21 +1,20 @@
+import { useEffect, useReducer } from 'react';
 import { initialTaskState } from './initialTaskState';
-import { useEffect, useState } from 'react';
+import { taskReducer } from './taskReducer';
 import { TaskContext } from './TaskContext';
-
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, setState] = useState(initialTaskState);
-
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
   useEffect(() => {
     console.log(state);
   }, [state]);
-  
+
   return (
-    <TaskContext.Provider value={{ state, setState }}>
+    <TaskContext.Provider value={{ state, dispatch }}>
       {children}
     </TaskContext.Provider>
   );

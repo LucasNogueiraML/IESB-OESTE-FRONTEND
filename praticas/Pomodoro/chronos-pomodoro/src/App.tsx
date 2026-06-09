2d85110108f462530421c89d27b075596ca8901d
LucasNogueiraML <lucas25070@gmail.com>
2026-06-09 12:12:00 -0300

    pratica 12 realizada

--- praticas/Pomodoro/chronos-pomodoro/src/App.tsx
+++ praticas/Pomodoro/chronos-pomodoro/src/App.tsx
@@ -1,9 +1,9 @@
-import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { Menu } from './components/Menu';
 export function App() {
   return (
     <>
@@ -11,7 +11,7 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
